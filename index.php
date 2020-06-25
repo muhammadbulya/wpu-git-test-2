f5759f4960d3666c179b5a1f7fc8ebae49394412
Muhammad Bulya <muhammadbulya22@gmail.com>
2020-06-25 08:25:21 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aperiam iusto facere perferendis dolorum unde molestias dignissimos, reiciendis nostrum vel ratione placeat architecto commodi maxime sit expedita harum dolor corporis.</p>
+<li>saru</li>
+
 </body>
 </html>
